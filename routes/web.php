43a846ce4b8c0ed868f39358f08518d607d4cfda
AWayOfHelp <awayofhelpofficial@gmail.com>
2022-05-10 17:21:16 +0530

    Completed with authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return view('home');
+})->name('home');
+Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+Route::get('/register',[RegisterController::class,'index'])->name('register');
+Route::post('/register',[RegisterController::class,'store']);
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login',[LoginController::class,'store']);
+Route::post('/logout',[LogoutController::class,'store'])->name('logout');
+Route::get('/posts', function () {
+    return view('post.index');
 });
